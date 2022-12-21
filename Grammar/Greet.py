544a54d5ffd3d0e7fd3f8e626a9cda6bebf0ce67
Akira Watanabe <a0323w@gmail.com>
2022-12-21 12:10:16 +0900

    文法関連

---
+++ Grammar/Greet.py
@@ -0,0 +1,14 @@
+name = 'aaa'
+
+print('こんにちは')
+print(f"Your name is {name}, right? \n Please answer with 'yes' or 'no'.")
+
+#以下のように記述することで、変数の型を指定することができる
+call: str = input()
+
+if call == "yes":
+    print(f"{name}, Nice to meet you.")
+elif call == "no":
+    print("Sorry, please tell me your name.")
+else:
+    print("Please answer with 'yes' or 'no'.")
