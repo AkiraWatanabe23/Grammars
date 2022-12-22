07a980ac5b18f16eb972a30b9c21b0995fad5743
Akira Watanabe <a0323w@gmail.com>
2022-12-22 13:39:11 +0900

    コメント追加(分かりにくい...)

--- Grammar/Copy.py
+++ Grammar/Copy.py
@@ -2,7 +2,15 @@
 import copy
 
 a = [1, 2, [3, 4]]
-print(type(a)) # -> <class 'list'>
+b = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
 
-b = copy(a)
-c = copy.deepcopy(a)
+print(type(a)) # -> <class 'list'>
+print(type(b)) # -> <class 'list'>
+
+"""
+浅いコピー ... 多次元リスト等の「深い要素」が存在し、その「深い要素」を変更する場合にコピー先と連動するもの
+深いコピー
+"""
+
