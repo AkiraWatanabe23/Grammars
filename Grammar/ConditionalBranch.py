1fcb46043a9d639873bd7190e2a93d39c33a3aa3
Akira Watanabe <a0323w@gmail.com>
2023-01-18 10:56:36 +0900

    入力の変更

--- Grammar/ConditionalBranch.py
+++ Grammar/ConditionalBranch.py
@@ -1,43 +1,34 @@
 #pythonの条件分岐について記述
-num = 100
+a, b = map(int, input().split())
 
-#1つの条件について判定する場合、以下のように「if 条件式:」のように記述する
 #記述した条件式がTrueだった場合に処理を実行する(後述の条件分岐は無視される)
-if num <= 10:
-    print("num は、10以下です")
+if a <= 10:
+    print("a は、10以下です")
 #前述までの条件式とは別の条件式での判定をする場合、「elif 条件式:」のように記述する
-elif num <= 50:
-    print("num は、50以下です")
-elif num <= 70:
-    print("num は、70以下です")
+elif a <= 50:
+    print("a は、50以下です")
+elif a <= 70:
+    print("a は、70以下です")
 #前述までの条件式が全てFalseだった場合の条件判定は、「else:」と記述する
 else:
-    print("num は、70より大きい数です")
+    print("a は、70より大きい数です")
     
 #複数の条件を同時に判定したい場合、「and」「or」を使って記述する
-a = 10
-b = 45
-
 if a >= 10 and b >= 50: # -> False
-    print("上記の条件を満たしています")
+    print("aは10以上、bは50以上の数です")
 else:
     print("上記の条件を満たしていません")
     
 if a >= 10 or b <= 50: # -> True
-    print("少なくとも上記の条件のうち片方は満たしています")
-    
-#「and」「or」は、2つ以上の条件を並べることも可能
+    print("aは10以上または、bは50以下の数です")
 
 #三項演算子を用いた条件分岐
-n = 50
-print("Yes" if n >= 20 else "No")
+print("bは20以上の数です" if b >= 20 else "bは20未満の数です")
 #print(a if c else b)
 
 #c ... 条件文
 #a ... Trueの場合の出力内容
 #b ... Falseの場合の出力内容
-
-#pythonには、他の言語に存在する「switch文」がない
 
 #pythonには、if, elif, else で簡単に記述できる、という理由でswitch文がない
 #python3.10以降から、新しく「match文」という構文が追加された
