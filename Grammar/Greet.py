7e4cd78cf8aaf0ddea9a0a0433e4c5d2a66e349a
Akira Watanabe <a0323w@gmail.com>
2023-01-24 10:57:18 +0900

    pylintに沿った修正

--- Grammar/Greet.py
+++ Grammar/Greet.py
@@ -1,15 +1,16 @@
-name = 'aaa'
+'''挨拶'''
+NAME = 'aaa'
 
 #以下のように、文字列は「" "」「' '」のどちらで記述してもよい
 print('Hello')
-print(f"Your name is {name}, right? \n Please answer with 'yes' or 'no'.")
+print(f"Your name is {NAME}, right? \n Please answer with 'yes' or 'no'.")
 
 #以下のように(変数名: 型名)記述することで、変数の型を指定することができる
 call: str = input()
 
 if call == "yes":
     #以下のformat(f"... { } ...")で記述することで、文字列の中に { } で変数を入れることができる
-    print(f"{name}, Nice to meet you.")
+    print(f"{NAME}, Nice to meet you.")
 elif call == "no":
     print("Sorry, please tell me your name.")
 else:
