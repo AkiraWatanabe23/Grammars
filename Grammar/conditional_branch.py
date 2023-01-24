617ee6e5feccd64123f4f6d1b9c1ce651f0d84f0
Akira Watanabe <a0323w@gmail.com>
2023-01-24 10:55:04 +0900

    pylintに沿った修正

--- Grammar/conditional_branch.py
+++ Grammar/conditional_branch.py
@@ -1,4 +1,4 @@
-#pythonの条件分岐について記述
+'''pythonの条件分岐について記述'''
 a, b = map(int, input().split())
 
 if a <= 10:
@@ -9,13 +9,14 @@ elif a <= 70:
     print("a は、70以下です")
 else:
     print("a は、70より大きい数です")
-    
+
 #論理演算
+#「and」複数の条件を全て満たしているか
 if a >= 10 and b >= 50:
     print("aは10以上、bは50以上の数です")
 else:
     print("上記の条件を満たしていません")
-    
+#「or」複数の条件の内、1つでも条件を満たしているか
 if a >= 10 or b <= 50:
     print("aは10以上または、bは50以下の数です")
 
@@ -29,17 +30,15 @@ print("bは20以上の数です" if b >= 20 else "bは20未満の数です")
 
 #pythonには、if, elif, else で簡単に記述できる、という理由でswitch文がない
 #python3.10以降から、新しく「match文」という構文が追加された
-name = "Ichiro"
+NAME = "Ichiro"
 
-match name:
+match NAME:
     case "Ichiro":
         print("He is a baseball player")
-
     case "Ronaldo":
         print("He is a soccer player")
-    
     case "Messi":
         print("He is a soccer player")
-
     case _:
         print("He is a professional player")
+        
--- Grammar/decorate_method.py
+++ Grammar/decorate_method.py
@@ -1,11 +1,11 @@
-#関数デコレータ...関数を修飾し、新しい関数を作成するもの(関数に付加機能を付けられる)
-#　　　　　　　　 複数の関数に対して同じ機能を付けたい時に使う
+'''関数デコレータ...関数を修飾し、新しい関数を作成するもの(関数に付加機能を付けられる)
+   複数の関数に対して同じ機能を付けたい時に使う'''
 #https://qiita.com/mtb_beta/items/d257519b018b8cd0cc2e
 #https://zenn.dev/ryo_kawamata/articles/learn_decorator_in_python
 
-#以下のように関数デコレータを定義(関数デコレータ内で関数を定義する)
-#ex.1)
+#関数デコレータの定義(関数デコレータ内で関数を定義する)
 def decorator(func_base): # <- 引数には、デコレートする関数(今回は「func()」)が入る
+    '''ex.1)引数を渡さない関数デコレータ'''
     def in_decolate():
         print('check', end=" ")
         func_base() # <- ここでデコレートする関数[func()]の処理を実行する
@@ -15,26 +15,26 @@ def decorator(func_base): # <- 引数には、デコレートする関数(今回
 # 下記の関数(「func()」)をデコレート(装飾)する、という意味
 @decorator
 def func():
+    '''デコレートされる関数'''
     print('decorate')
 
 @decorator
 def func_test():
+    '''デコレートされる関数'''
     print("test")
 
 func()      # -> check decorate
 func_test() # -> check test
 
-#ex.2)引数を受け取る関数デコレータ
 #def base(): ... デコレータの引数を受け取る
 def base(get): # <- get...関数デコレータの引数(「@...」の時に設定する)
-
+    '''ex.2)引数を受け取る関数デコレータ'''
     #def child(): ... デコレートする関数を受け取る
-    def child(func): # <- func...デコレートする関数
+    def child(func_child): # <- func...デコレートする関数
 
         #def wrap(): ... 実際の処理を記述する
         def wrap(*args, **kwargs): # <- ここには、「func()」の引数が入る
-            
-            x = func(*args, **kwargs)
+            x = func_child(*args, **kwargs)
             return f"<{get}> {x} </{get}>" # <- wrap()のreturn
 
         return wrap # <- child()のreturn
@@ -43,6 +43,7 @@ def base(get): # <- get...関数デコレータの引数(「@...」の時に設�
 
 @base("h")
 def greet():
+    '''引数を渡してデコレートされる'''
     return 'Hello World'
 
 print(greet()) # -> <h> Hello World </h>
